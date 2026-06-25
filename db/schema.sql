567f2bbd00427c8a32dfbb4ffdcc22ba11005908
Diego Mosquera <diegomosquerahernandez@gmail.com>
2026-06-25 10:37:00 -0500

    diagramas y mapas de uso, se elimina tarjeta de compra de prueba , de html y de db

--- db/schema.sql
+++ db/schema.sql
@@ -128,7 +128,6 @@ INSERT IGNORE INTO productos (nombre, precio, precio_original, stock, categoria,
 ('Gorra Negra YJ Dorada',     90000, 90000, 10, 'negra',         'gorra-negra-yj-dorado.jpg'),
 ('Gorra Negra YJ Firma Dorada',90000, 90000, 10, 'negra',        'gorra-negra-yj-firma-dorada.jpg'),
 ('Gorra Azul Turqui',         90000, 95000, 10, 'azul',          'gorra-azulturqui1.jpg'),
-('Prueba',                     5000,  5000, 10, 'prueba',        'gorra-negra.jpg'),
 ('Gorras Personalizadas',         0,     0,  0, 'personalizada', 'personalizada1.jpg');
 
 -- ============================================================
